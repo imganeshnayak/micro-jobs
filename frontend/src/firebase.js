c13d9056a9ffd47ab8b05e9b2b5147ccf4d01719
unknown <itzganeshnayak@gmail.com>
2025-02-01 23:53:38 +0530

    testings

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider,signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBDdoX5In7PelvC9Vn8p4p0eRr6zfPSw7U",
-  authDomain: "dooq-7e291.firebaseapp.com",
-  projectId: "dooq-7e291",
-  storageBucket: "dooq-7e291.firebasestorage.app",
-  messagingSenderId: "105904541632",
-  appId: "1:105904541632:web:1697928b3a2558f0c84225",
-  measurementId: "G-M6WBBGWFVG"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
