777bf68ad2d4d7b8888ce858f6b02894dc0aed27
unknown <itzganeshnayak@gmail.com>
2025-02-08 20:32:59 +0530

    testings21

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",  // Ensure correct asset paths in production
   build: {
     outDir: 'dist',
     emptyOutDir: true,
